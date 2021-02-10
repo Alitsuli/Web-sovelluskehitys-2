ffcd1ff3e8e9ad60f90dc893afdb4f49270b7a13
altsuli <ali_reza222@hotmail.com>
2021-02-10 20:33:36 +0200

    REST & mashupin jatkamista

--- src/ladataan.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { Spinner } from "react-bootstrap";
-
-function Loading(props) {
-  return <Spinner id="spinner" animation="grow" role="status"></Spinner>;
-}
-
-export default Loading;
